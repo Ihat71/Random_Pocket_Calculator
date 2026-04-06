11ab0583958585ef9e54316908367fe0af6c567c
Mohammed Najmaldeen <145692939+Ihat71@users.noreply.github.com>
2026-04-06 19:02:58 +0300

    Delete Java-codes directory

--- Java-codes/deleteBeg.java
+++
@@ -1,5 +0,0 @@
-public class deleteBeg {
-    public static void main(String[] args){
-        
-    }
-}
--- Java-codes/qeue.java
+++
@@ -1,5 +0,0 @@
-public class qeue {
-    public static void main(String[] args){
-        
-    }
-}
